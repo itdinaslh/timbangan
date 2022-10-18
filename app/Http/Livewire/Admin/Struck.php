71947090fec8b06bebf38beb09323d68c98d9816
itdinaslh <itdinaslh@outlook.com>
2022-10-18 16:12:51 +0700

    update struck

--- app/Http/Livewire/Admin/Struck.php
+++ app/Http/Livewire/Admin/Struck.php
@@ -19,19 +19,20 @@ class Struck extends Component
         $data = DB::connection('bg_db')->table('transaction_log')
             ->select([
                 'trans_date', 'id', 'trans_date_after', 'door_id','truck_id','weight','weight_after'
-            ])
+            ])->where('id', $this->search)
             // ->when($this->filter, function ($builder) {
             //     $builder->where('status', $this->filter);
             // })
-            ->when($this->search, function ($builder) {
-                $builder->where(function ($builder) {
-                    $builder->where('id', $this->search);
-                        // ->orWhere('trans_date', 'like', '%' . $this->search . '%')
-                        // ->orWhere('trans_date_after', 'like', '%' . $this->search . '%')
-                        // ->orWhere('door_id', 'like', '%' . $this->search . '%')
-                        // ->orWhere('truck_id', 'like', '%' . $this->search . '%');
-                });
-            })->orderBy('id' ,'desc')
+            // ->when($this->search, function ($builder) {
+            //     $builder->where(function ($builder) {
+            //         $builder->where('id', $this->search);
+            //             // ->orWhere('trans_date', 'like', '%' . $this->search . '%')
+            //             // ->orWhere('trans_date_after', 'like', '%' . $this->search . '%')
+            //             // ->orWhere('door_id', 'like', '%' . $this->search . '%')
+            //             // ->orWhere('truck_id', 'like', '%' . $this->search . '%');
+            //     });
+            // })
+            ->orderBy('id' ,'desc')
         ->paginate($this->show);
   
         return view('livewire.admin.struck', ['data' => $data]);
