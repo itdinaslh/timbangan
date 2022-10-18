8a0192ddcc2dcd01602206d7e17f6575eff36877
itdinaslh <itdinaslh@outlook.com>
2022-10-18 16:26:54 +0700

    Updated Truck Transaction

--- app/Http/Livewire/Admin/Struck.php
+++ app/Http/Livewire/Admin/Struck.php
@@ -16,22 +16,14 @@ class Struck extends Component
     
     public function render()
     {
+        $slick = $this->search;
         $data = DB::connection('bg_db')->table('transaction_log')
             ->select([
                 'trans_date', 'id', 'trans_date_after', 'door_id','truck_id','weight','weight_after'
-            ])->where('id', $this->search)
-            // ->when($this->filter, function ($builder) {
-            //     $builder->where('status', $this->filter);
-            // })
-            // ->when($this->search, function ($builder) {
-            //     $builder->where(function ($builder) {
-            //         $builder->where('id', $this->search);
-            //             // ->orWhere('trans_date', 'like', '%' . $this->search . '%')
-            //             // ->orWhere('trans_date_after', 'like', '%' . $this->search . '%')
-            //             // ->orWhere('door_id', 'like', '%' . $this->search . '%')
-            //             // ->orWhere('truck_id', 'like', '%' . $this->search . '%');
-            //     });
-            // })
+            ])
+            ->when($slick != "", function($query) use ($slick) {
+                return $query->where('id', 'like', $this->search . '%');
+            })
             ->orderBy('id' ,'desc')
         ->paginate($this->show);
   
